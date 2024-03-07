3c08daf6f2b9494a325f8f32088f61a41ca892be
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-03-06 17:58:06 -0700

    Tests file created and first test done for the triangle class

---
+++ tests/shapes.test.js
@@ -0,0 +1,20 @@
+const ShapesTriangle = require('../lib/triangle.js');
+// const ShapesCircle = require('../lib/circle');
+// const ShapesSquare = require('../lib/square');
+
+describe('Triangle', () => {
+    describe('render method', () => {
+        it('should return a string containing a polygon with the text from the input', () => {
+            const triangle = new ShapesTriangle('abc', 'black', 'red', 'blue');
+            triangle.textColor = 'black';
+            triangle.shapeColor = 'red';
+            triangle.borderColor = 'blue';
+            // Debugging purposes
+            console.log(triangle.render());
+            // This line removes newlines and indentation from the received string
+            const receivedSVG = triangle.render().replace(/\n\s*/g, '');
+            const expectedSVG = `<svg height="200" width="300" xmlns="http://www.w3.org/2000/svg"><polygon points="160 0, 250 200, 40 200" style="fill:red; stroke-width:3; stroke:blue" /><text x="150" y="150" text-anchor="middle" fill="black" font-size="50" font-family="Helvetica">abc</text></svg>`;
+            expect(receivedSVG).toEqual(expectedSVG);
+        });
+    });
+});
